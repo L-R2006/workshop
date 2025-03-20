64cd2fe421bb0c4f5f13d0550c6df1db6600b9c2
L-R2006 <lizi.rakviashvili.1@iliauni.edu.ge>
2025-03-20 11:34:48 +0400

    Create Assigment01.java

---
+++ Assigment01.java
@@ -0,0 +1,9 @@
+public class Assigment01 {
+    public static void main(String[] args) {
+
+        int A = 17;
+        int B = 18;
+
+        System.out.println("The sum of the numbers is " + (A + B));
+    }
+}
